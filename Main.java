3278de0b191bdb234c4f9b1046ce96cf1372fee1
Soni19999 <43901593+Soni19999@users.noreply.github.com>
2018-10-06 14:37:47 +0530

    Update and rename Positive.java to Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 import java.util.*;
  
-public class Positive
+public class Main
 {
     public static void main(String []s)
     {
-        int num;
+        int n;
         
         Scanner sc=new Scanner(System.in);
          
-        System.out.print("Enter any integer number: ");
-        num=sc.nextInt();
+        
+        n=sc.nextInt();
         
         
         
-        if(num>0)
+        if(n>0)
             System.out.println(" POSITIVE ");
-        else if(num<0)
+        else if(n<0)
             System.out.println(" NEGATIVE ");
         else
             System.out.println(" ZERO");
